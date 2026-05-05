9f2f16fd6dfec9ed21efacc6a4f869b0708488b7
Uday Ige <uday.ige@amahahealth.com>
2026-05-05 05:38:24 +0000

    Added Rack: middleware

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Todo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    config.middleware.use Rack::Attack
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
